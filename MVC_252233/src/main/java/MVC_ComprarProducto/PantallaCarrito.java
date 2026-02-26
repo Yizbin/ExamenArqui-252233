bfbdf6f528215a62dd88d0630366c88235f6d06f
Yizbin <148611835+Yizbin@users.noreply.github.com>
2026-02-25 18:59:16 -0600

    Diagramas de disenio y estructura

---
+++ MVC_252233/src/main/java/MVC_ComprarProducto/PantallaCarrito.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MVC_ComprarProducto;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Abraham Coronel
+ */
+public class PantallaCarrito extends JFrame implements ISuscriptor {
+
+    public PantallaCarrito() {
+    }
+
+    @Override
+    public void update() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
